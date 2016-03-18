f6386a8f2150a16d83758cb4a50f7bc2b9e41b50
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-18 07:02:48 -0700

    Update english_lang.lua

--- plugins/english_lang.lua
+++ plugins/english_lang.lua
@@ -54,11 +54,13 @@ local function run(msg, matches)
 		set_text(LANG, 'noStickersL', 'Stickers are not allowed in this supergroup.')
 		set_text(LANG, 'stickersT', 'Stickers are now allowed in this chat.')
 		set_text(LANG, 'stickersL', 'Stickers are now allowed in this supergroup.')
-
-                set_text(LANG, 'noLinksT', 'Links are not allowed in this chat.')
-                set_text(LANG, 'noLinksL', 'Links are not allowed in this supergroup.')
+        
+         
+         
                 set_text(LANG, 'linksT', 'Links are not allowed in this chat.')
                 set_text(LANG, 'linksL', 'Links are not allowed in this supergroup.')
+                set_text(LANG, 'noLinksT', 'Links are not allowed in this chat.')
+                set_text(LANG, 'noLinksL', 'Links are not allowed in this supergroup.')
                 
                 
 		set_text(LANG, 'gifsT', 'Gifs are now allowed in this chat.')
