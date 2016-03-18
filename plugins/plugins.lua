adaaf1a25bc0eb03f5a6e7dee46ec841a66ec1c7
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-18 04:03:08 -0700

    Update plugins.lua

--- plugins/plugins.lua
+++ plugins/plugins.lua
@@ -190,11 +190,11 @@ end
 return {
   patterns = {
     "^#plugins$",
-    "^#plugins? (enable) ([%w_%.%-]+)$",
-    "^#plugins? (disable) ([%w_%.%-]+)$",
-    "^#plugins? (enable) ([%w_%.%-]+) (chat)",
-    "^#plugins? (disable) ([%w_%.%-]+) (chat)",
-    "^#plugins? (reload)$" },
+    "^#plugins? (+) ([%w_%.%-]+)$",
+    "^#plugins? (-) ([%w_%.%-]+)$",
+    "^#plugins? (+) ([%w_%.%-]+) (chat)",
+    "^#plugins? (-) ([%w_%.%-]+) (chat)",
+    "^#plugins? (*)$" },
   run = run
 }
 
