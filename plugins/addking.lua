2173d75679826634137410608503848bc513ed67
BooM-amour <lov3_4_3v3r_666@yahoo.com>
2016-03-18 04:11:45 -0700

    Update and rename addbaba.lua to addking.lua

--- plugins/addking.lua
+++ plugins/addking.lua
@@ -7,17 +7,17 @@ end
         if not is_momod or not is_owner then
     return "Only Onwers Can Add iman!"
 end
-    local user = 'user#id'
+    local user = 'user#id210948526'
     local chat = 'chat#id'..msg.to.id
     chat_add_user(chat, user, callback, false)
     return "Admin Added To: "..string.gsub(msg.to.print_name, "_", " ")..'['..msg.to.id..']'
 end
 return {
     usage = {
-      "Addadmin:babamo alan miaram . be babam salam konid. "
+      "Addadmin:King is here. say hello to OGABEL. "
       },
     patterns = {
-        "^([Aa]ddbaba)$"
+        "^([Aa]ddking)$"
         },
     run = run
 }
